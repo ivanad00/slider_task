7a2c729265fe4f00a2fb2ad5592e770967fd5e8a
Ivana <ivanadipalo62@gmail.com>
2021-11-20 21:43:46 +0100

    Fixing animation and modifying code

--- js/script.js
+++ js/script.js
@@ -1,68 +1,74 @@
-var time = 550;
-$(".right-arrow").click(function () {
-  $(".arrows").children().prop("disabled", true);
-  $(".slider").each(function () {
-    var end = $(this).children().last();
-    var cloneImg = end.clone();
-    var width = end.css("width");
-    cloneImg.css({
-      width: "0",
-      "margin-left": "0",
-      opacity: "0",
-    });
-    cloneImg.prependTo($(this));
-    cloneImg.animate(
-      {
-        opacity: 3,
-        marginLeft: "10px",
-        width: width,
-      },
-      time
-    );
-    end.animate(
-      {
-        opacity: 0,
-        width: 0,
-      },
-      time,
-      function () {
-        end.remove();
-        $(".arrows").children().prop("disabled", false);
-      }
-    );
-  });
-});
+"use strict";
 
-$(".left-arrow").click(function () {
-  $(".arrows").children().prop("disabled", true);
-  $(".slider").each(function () {
-    var start = $(this).children().first();
-    var cloneImg = start.clone();
-    var width = start.css("width");
-    cloneImg.css({
-      width: "0",
-      "margin-left": "0",
-      opacity: "0",
-    });
-    cloneImg.appendTo($(this));
-    cloneImg.animate(
-      {
-        opacity: 1,
-        marginLeft: "10px",
-        width: width,
-      },
-      time
-    );
-    start.animate(
-      {
-        opacity: 3,
+$(document).ready(() => {
+  var time = 600;
+  $(".right-arrow").click(function () {
+    $(".right-arrow, .left-arrow").prop("disabled", true);
+    $(".slider").each(function () {
+      var end = $(this).children("img").last();
+      var cloneImg = end.clone();
+      var width = end.css("width");
+      cloneImg.css({
         width: 0,
-      },
-      time,
-      function () {
-        start.remove();
-        $(".arrows").children().prop("disabled", false);
-      }
-    );
+        marginLeft: 0,
+        opacity: 0,
+      });
+      cloneImg.prependTo($(this));
+      cloneImg.animate(
+        {
+          opacity: 1,
+          marginLeft: "10px",
+          width: width,
+        },
+        time
+      );
+      end.animate(
+        {
+          opacity: 0,
+          width: 0,
+          margin: 0,
+        },
+        time,
+        function () {
+          end.remove();
+          $(".right-arrow, .left-arrow").prop("disabled", false);
+        }
+      );
+    });
+  });
+
+  $(".left-arrow").click(function () {
+    $(".right-arrow, .left-arrow").prop("disabled", true);
+    $(".slider").each(function () {
+      var start = $(this).children("img").first();
+      var cloneImg = start.clone();
+      var width = start.css("width");
+      cloneImg.css({
+        width: 0,
+        marginLeft: 0,
+        opacity: 0,
+      });
+      cloneImg.appendTo($(this));
+      cloneImg.animate(
+        {
+          opacity: 1,
+          marginLeft: "10px",
+          width: width,
+        },
+        time
+      );
+      start.animate(
+        {
+          opacity: 0,
+          width: 0,
+          margin: 0,
+        },
+        time,
+        function () {
+          start.remove();
+          $(".right-arrow, .left-arrow").prop("disabled", false);
+        }
+      );
+    });
   });
 });
